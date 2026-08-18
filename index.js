2f3cb442e64ecadd54f8ddc5899045bee6a6c566
Samridhi Khanna <samridhi18khanna@gmail.com>
2026-08-18 10:51:40 +0530

    Resume

--- index.js
+++ index.js
@@ -49,6 +49,58 @@
 
 //loginHandler("Samridhi","1825",login);
 
-console.log("ONE")
-setTimeout(() => {console.log("TWO")}, 2000);
-console.log("THREE")
+//console.log("ONE");
+//for(i =0; i<10000; i++)
+//{
+//console.log("i="+i);
+//}
+//setTimeout(() => {console.log("TWO")}, 2000);
+//console.log("THREE")
+
+const container = document.getElementById('container');
+const btn = document.getElementById('btn');
+     //const loader = document.createElement('h2');
+     //container.appendChild(loader);
+
+     const h1= document.createElement('h1');
+     console.log(h1);
+     h1.innerText = "Education";
+
+     const h4= document.createElement('h4');
+     console.log(h4);
+     h4.innerText = "B.Tech in Computer Science and Engineering\nABES ENGINEERING COLLEGE (AFFILIATED TO AKTU)\nCGPA: 8.78 (Sem I), 9.14 (Sem II)\nISCE/ISC (Class X & XII)\nST. FIDELIS COLLEGE- LUCKNOW\n95 percentile (Class X), 92 percentile (Class XII)\nSubject Topper — Mathematics and English";
+    
+
+     const h11= document.createElement('h1');
+     console.log(h11);
+     h11.innerText = "Technical Skills";
+
+     const h41= document.createElement('h4');
+     console.log(h41);
+     h41.innerText ="Languages: Java, C++\nfrontend: HTML, CSS, UI/UX Design\nCore CS: Data Structures & Algorithms, OOPs, Problem Solving\nTools: Git, Github, Canva";
+//console.log(container);
+//console.log(btn);
+
+
+
+        function ping(){
+          //try{
+            //loader.innerHTML = "<h2>Loading...</h2>";
+            //container.innerHTML = '<h2 style="color:red">Welcome to DOM</h2>';
+            //container.appendChild(h1);
+          //}catch(e){
+            //loader.innerHTML ='<h2 style="color:red">Server is down</h2>';
+          //}
+          //finally{
+            //loader.innerHTML = "";
+          //}   
+                 //alert('server ping')
+          container.innerHTML ="<h1 style='text-align: center;'>Samridhi Khanna- Resume</h1>";
+          container.appendChild(h1);
+          container.appendChild(h4);
+          const hr = document.createElement('hr');
+          container.appendChild(hr);
+          container.appendChild(h11);
+          container.appendChild(h41);
+}
+btn.addEventListener('click', ping);
